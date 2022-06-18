1098c0b39d60b584539f8cd6b966087fbb890e96
Giorgio Pogliani <giorgio.pogliani@performingdigital.com>
2022-06-18 13:56:48 +0200

    Fix .env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Dotenv\Dotenv;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (! file_exists(base_path() . DIRECTORY_SEPARATOR . '.env')) {
+            $env = Dotenv::createImmutable(getEnvPath())->load();
+
+            config()->set('app.app_key', $env['app_key']);
+            config()->set('app.app_secret', $env['app_secret']);
+            config()->set('app.consumer_key', $env['consumer_key']);
+        }
+
         app()->singleton('ovh', function(){
             return new \Ovh\Api(
                 config('app.app_key'),
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,21 +17,6 @@ $app = new LaravelZero\Framework\Application(
 
 /*
 |--------------------------------------------------------------------------
-| Set the correct path for the environment file
-|--------------------------------------------------------------------------
-|
-| The default environment file is usually located within the project root
-| directory. However, once you export the build of your application
-| you can move the environment file to `~/.config/ovhcli/.env`.
-|
-*/
-
-if (! file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . '.env')) {
-    $app->useEnvironmentPath(getEnvPath());
-}
-
-/*
-|--------------------------------------------------------------------------
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
 |
